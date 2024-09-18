5c71f680cf01c356ff5c27a5c4653d68430b9515
billy clark <billy.clark@presciencelabs.org>
2024-09-18 14:04:34 -0400

    structure refactoring
    needed to handle situations where the word and sign are not the same, e.g., Like and like.png.

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const contents = await Bun.stdin.text()
 
 const lines = extract_lines(contents)
 
-const content = transform(lines)
+const content = await transform(lines)
 
 const def: DocumentDefinition = {
 	fonts: {
@@ -22,10 +22,30 @@ const def: DocumentDefinition = {
 
 await Bun.write(Bun.stdout, await makePdf(def))
 
-function transform(lines: string[]): Block[] {
+function extract_lines(input: string): string[] {
+	return contents.split('\n').map(remove_win_remnants).filter(valid)
+
+	function valid(line: string = '') {
+		return !['#', ''].includes(line.trim().charAt(0))
+	}
+
+	function remove_win_remnants(line: string) {
+		return line.replace(/\r/g, '')
+	}
+}
+
+type WordSign = {
+	word: string
+	image: string
+}
+
+async function transform(lines: string[]): Promise<Block[]> {
 	const blocks = []
 	const MAX_SIGNS_PER_LINE = 5
-	const SPACER = ''
+	const SPACER: WordSign = {
+		word: '',
+		image: 'images/_placeholder.png',
+	}
 	const PAGE_BREAK: TextBlock = {
 		text: '',
 		breakAfter: 'always',
@@ -34,51 +54,61 @@ function transform(lines: string[]): Block[] {
 	for (const line of lines) {
 		const words = line.split(' ')
 
-		const buffer: string[] = []
+		const buffer: WordSign[] = []
 		for (const word of words) {
-			buffer.push(word)
+			buffer.push({
+				word,
+				image: await derive_image(word),
+			})
 
 			if (buffer.length === MAX_SIGNS_PER_LINE) {
-				blocks.push(...flush(buffer))
+				blocks.push(...await flush(buffer))
 
 				buffer.length = 0
 			}
 		}
 
-		// this ensures lines are left-aligned since these are a mix of a sign and a word
+		// this ensures lines are left-aligned since these are a combo of a sign and a word
 		while (buffer.length && buffer.length < MAX_SIGNS_PER_LINE) {
 			buffer.push(SPACER)
 		}
 
-		blocks.push(...flush(buffer), PAGE_BREAK)
+		blocks.push(... await flush(buffer), PAGE_BREAK)
 	}
 
 	return blocks
-}
 
-function extract_lines(input: string): string[] {
-	return contents.split('\n').filter(valid)
+	async function derive_image(word: string): Promise<string> {
+		const ENDS_IN_PLURAL_OR_PUNCT = /[s.]$/
+		if(ENDS_IN_PLURAL_OR_PUNCT.test(word)) {
+			return await derive_image(word.slice(0, -1))
+		}
 
-	function valid(line: string = '') {
-		return !['#', ''].includes(line.trim().charAt(0))
+		const sign = `images/${word}.png`
+
+		if (await Bun.file(sign).exists()) {
+			return sign
+		}
+
+		const sign_lowercase = sign.toLowerCase()
+		if (await Bun.file(sign_lowercase).exists()) {
+			return sign_lowercase
+		}
+
+		return 'images/_missing.png'
 	}
 }
 
-
-function flush(words: string[] = []): ColumnsBlock[] {
+async function flush(word_signs: WordSign[] = []): Promise<ColumnsBlock[]> {
 	return [
 		{
-			columns: words.map(word => ({
-				image: `images/${word || '_placeholder'}.png`,
-			})),
+			columns: word_signs.map(({image}) => ({image})),
 		},
 		{
-			columns: words.map(word => ({
-				text: word
-			})),
+			columns: word_signs.map(({word}) => ({text: word})),
 			textAlign: 'center',
 			padding: {
-				bottom: 30,
+				bottom: 50,
 			},
 		},
 	]
